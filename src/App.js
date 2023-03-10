9b28984a1af40f7ef392d5924c830995bdbf15f6
ckq-aws <cquarcoo@amazon.com>
2023-03-10 09:16:37 -0500

    new file:   .gitignore 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/App.test.js 	new file:   src/Hello.js 	new file:   src/index.css 	new file:   src/index.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Greet from './Hello';
+
+function App() {
+  return (
+    <div className="App">
+     <Greet /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,7 @@
+// import { Component } from "react";
+
+function Greet() {
+    return <h1>Hello World!</h1>
+}
+
+export default Greet
