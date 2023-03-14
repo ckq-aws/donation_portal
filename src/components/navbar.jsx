a50f2e85a58aefc925920fcf384996ff14d0ca18
ckq-aws <cquarcoo@amazon.com>
2023-03-14 12:27:20 -0400

    re-ordered the orgs on the donate page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Greet from './Hello';
+
 
 
 function NavToolBar() {
@@ -17,14 +17,10 @@ function NavToolBar() {
             <Nav.Link href="/orgs">Donate</Nav.Link>
             <NavDropdown title="More" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
--- src/components/organizations.js
+++ src/components/organizations.js
@@ -7,19 +7,17 @@ function Organizations({orgs}) {
   return (
     <>
         {Orgs.map(org => (
-          <Container>
-            <Row>
-              <div >
-                  <Card key={org}>
-                    <Card.Img variant='bottom' src="https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"/>
-                    <Card.Title>Organizaiton: {org.organization_name}</Card.Title>
-                    <Card.Body>Sector: {org.sector}</Card.Body>
-                    <Card.Text>This non-profit works for the good of others who can't fight for themselves.</Card.Text>
-                    <Button>Support This Cause!</Button>
-                  </Card>
-              </div>
+            <Row xs={1} md={2} className="g-4">
+              {Array.from({ length: 3 }).map((_, idx) =>(
+                <Card key={org} border="primary" style={{ width: '18rem' }}>
+                <Card.Img variant='bottom' src="https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80"/>
+                <Card.Title>Organizaiton: {org.organization_name}</Card.Title>
+                <Card.Body>Sector: {org.sector}</Card.Body>
+                <Card.Text>This non-profit works for the good of others who can't fight for themselves.</Card.Text>
+                <Button>Support This Cause!</Button>
+              </Card>
+              ))}
             </Row>
-          </Container>
         ))}
     </>
   )
